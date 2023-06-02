45dd7a57f029c23cd4809f65f591f1b685f53db1
AndersonRichard <richard-r3@hotmail.com>
2023-06-02 18:07:22 -0300

    Adicionado o exercicio 1004

---
+++ Java/src/iniciante/Questao1004.java
@@ -0,0 +1,14 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class Questao1004 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int PROD = a * b;
+        System.out.println("PROD = " + PROD);
+    }
+}
