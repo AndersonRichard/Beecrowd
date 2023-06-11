855eae3a8310aa94cea507cb43a2aa2a6207bc30
AndersonRichard <richard-r3@hotmail.com>
2023-06-11 20:09:39 -0300

    Adiciona Questao1007

---
+++ Java/src/iniciante/Questao1007.java
@@ -0,0 +1,15 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class Questao1007 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int diferenca = ((a *b) - (c * d));
+        System.out.println("DIFERENCA = " + diferenca);
+    }
+}
